599659526a5612a7ae0f00bb4fc97396ccf37709
Rowan Johns <120813329+RowanJohns@users.noreply.github.com>
2024-10-15 16:51:54 +0100

    started "distributor" task
    events part of step 1

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,7 @@
 package gol
 
+import "uk.ac.bris.cs/gameoflife/util"
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -16,13 +18,17 @@ func distributor(p Params, c distributorChannels) {
 	world := make([][]byte, p.ImageHeight)
 	for y := 0; y < p.ImageHeight; y++ {
 		world[y] = make([]byte, p.ImageWidth)
+	}
 
 	turn := 0
 	c.events <- StateChange{turn, Executing}
 
-	// TODO: Execute all turns of the Game of Life.
-
-	// TODO: Report the final state using FinalTurnCompleteEvent.
+	// TODO: complete all turns for GOL
+	// read initial world from input channel!!
+	
+	// TODO: report the final state using FinalTurnCompleteEvent.
+	aliveCells := []util.Cell{}
+	c.events <- FinalTurnComplete{turn, aliveCells}
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
