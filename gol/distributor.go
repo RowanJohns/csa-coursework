4d57400b537994becb322480f5d9742a2c312ffa
arshia1453 <wj23237@bristol.ac.uk>
2024-10-17 15:36:48 +0100

    game of life implementation 1

--- gol/distributor.go
+++ gol/distributor.go
@@ -30,12 +30,59 @@ func distributor(p Params, c distributorChannels) {
 			world[i][j] = <-c.ioInput //creating the initial world
 		}
 	}
-	
-	for turn < p.Turns {
 
+	for turn < p.Turns {
+		// Create the next world state (same dimensions as the current world)
+		nextWorld := make([][]byte, p.ImageHeight)
+		for i := range world {
+			nextWorld[i] = make([]byte, p.ImageWidth) // Initialize each row of the new world
+		}
+
+		// Iterate over each cell in the world
+		for i := 0; i < p.ImageHeight; i++ {
+			for j := 0; j < p.ImageWidth; j++ {
+				// Calculate the sum of the live neighbors
+				sum := int(world[(i+p.ImageHeight-1)%p.ImageHeight][(j+p.ImageWidth-1)%p.ImageWidth]) +
+					int(world[(i+p.ImageHeight-1)%p.ImageHeight][(j+p.ImageWidth)%p.ImageWidth]) +
+					int(world[(i+p.ImageHeight-1)%p.ImageHeight][(j+p.ImageWidth+1)%p.ImageWidth]) +
+					int(world[(i+p.ImageHeight)%p.ImageHeight][(j+p.ImageWidth-1)%p.ImageWidth]) +
+					int(world[(i+p.ImageHeight)%p.ImageHeight][(j+p.ImageWidth+1)%p.ImageWidth]) +
+					int(world[(i+p.ImageHeight+1)%p.ImageHeight][(j+p.ImageWidth-1)%p.ImageWidth]) +
+					int(world[(i+p.ImageHeight+1)%p.ImageHeight][(j+p.ImageWidth)%p.ImageWidth]) +
+					int(world[(i+p.ImageHeight+1)%p.ImageHeight][(j+p.ImageWidth+1)%p.ImageWidth])
+
+				sum /= 255 // Normalize sum since alive cells are 255 (dead are 0)
+
+				// Apply the Game of Life rules
+				if world[i][j] == 255 { // Cell is alive
+					if sum < 2 || sum > 3 {
+						nextWorld[i][j] = 0 // Cell dies
+					} else {
+						nextWorld[i][j] = 255 // Cell stays alive
+					}
+				} else { // Cell is dead
+					if sum == 3 {
+						nextWorld[i][j] = 255 // Cell becomes alive
+					} else {
+						nextWorld[i][j] = 0 // Cell stays dead
+					}
+				}
+			}
+		}
+		world = nextWorld
 	}
 	// TODO: report the final state using FinalTurnCompleteEvent.
 	aliveCells := []util.Cell{}
+
+	for i, row := range world {
+		for j := range row {
+			// If the cell is alive (i.e., has a value of 1)
+			if world[i][j] == 255 {
+				// Add the alive cell's coordinates (x, y) to the list of alive cells
+				aliveCells = append(aliveCells, util.Cell{j, i})
+			}
+		}
+	}
 	c.events <- FinalTurnComplete{turn, aliveCells}
 
 	// Make sure that the Io has finished any output before exiting.
